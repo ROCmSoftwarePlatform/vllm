8d7d2a18c3e338b578c6073acec84246343f6d9a
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-08-12 17:19:48 +0000

    Missing import

--- vllm/config.py
+++ vllm/config.py
@@ -11,7 +11,8 @@ from vllm.logger import init_logger
 from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 from vllm.model_executor.models import ModelRegistry
 from vllm.transformers_utils.config import get_config, get_hf_text_config
-from vllm.utils import get_cpu_memory, is_cpu, is_hip, is_neuron
+from vllm.utils import (get_cpu_memory, is_cpu, is_hip, is_neuron,
+                        print_warning_once)
 
 if TYPE_CHECKING:
     from ray.util.placement_group import PlacementGroup
