69a6d827d229948093ac786b312d78d27ccd0c83
shsanyal <shomy.sanyal@amd.com>
2024-08-14 10:23:30 +0000

    div round up clang-format

--- csrc/custom/paged_attention/attention_ll4mi.cu
+++ csrc/custom/paged_attention/attention_ll4mi.cu
@@ -13,7 +13,7 @@
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define DIVIDE_ROUND_UP(a, b) (((a) + (b)-1) / (b))
+#define DIVIDE_ROUND_UP(a, b) (((a) + (b) - 1) / (b))
 #define WARP_SIZE 64
 
 #if defined(__HIP__MI300__)  // TODO: Add NAVI support
