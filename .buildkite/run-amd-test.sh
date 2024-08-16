240c91bd8c365a9d57a7bcf6e140b2857807e757
omkarkakarparthi <okakarpa>
2024-08-16 18:34:38 +0000

    testing ignore it

--- .buildkite/run-amd-test.sh
+++ .buildkite/run-amd-test.sh
@@ -43,23 +43,25 @@ done
 
 echo "--- Building container"
 sha=$(git rev-parse --short HEAD)
-image_name=rocm_${sha}
-container_name=rocm_${sha}_$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 10; echo)
-docker build \
-        -t ${image_name} \
-        -f Dockerfile.rocm \
-        --progress plain \
-        .
+image_name=rocm/pytorch:latest
+container_name=dummy
+echo "$RENDER_DEVICES"
+#rocm_${sha}_$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 10; echo)
+#docker build \
+#        -t ${image_name} \
+#        -f Dockerfile.rocm \
+#        --progress plain \
+#        .
 
-remove_docker_container() {
-   docker rm -f ${container_name} || docker image rm -f ${image_name} || true
-}
-trap remove_docker_container EXIT
+#remove_docker_container() {
+#   docker rm -f ${container_name} || docker image rm -f ${image_name} || true
+#}
+#trap remove_docker_container EXIT
 
 echo "--- Running container"
 
 docker run \
-        --device /dev/kfd --device /dev/dri \
+        --device /dev/kfd --device $RENDER_DEVICES \
         --network host \
         --rm \
         -e HF_TOKEN \
