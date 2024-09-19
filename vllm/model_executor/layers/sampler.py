c14a050681e6db35623c88744c750bdf52f9a418
Alexei V. Ivanov <alexei.ivanov@amd.com>
2024-09-19 17:42:29 +0000

    Formatting.

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -825,7 +825,7 @@ def _sample_with_torch(
         seq_groups = [sampling_metadata.seq_groups[i] for i in seq_group_id]
         sample_metadata[sampling_type] = (seq_group_id, seq_groups)
         long_sample_indices = sample_indices.long()
-        
+
         if sampling_type == SamplingType.GREEDY:
             greedy_samples = torch.argmax(logprobs[long_sample_indices],
                                           dim=-1)
