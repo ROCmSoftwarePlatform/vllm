f1e28dde73b8a1dd1b52e8b5175fac29f0c2ed2d
Shomy <shomy.sanyal@amd.com>
2024-10-08 17:46:55 +0000

    isort fix

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -27,6 +27,7 @@ import torch
 from torch import nn
 from transformers import LlamaConfig
 
+import vllm.envs as envs
 from vllm import _custom_ops as ops
 from vllm.attention import Attention, AttentionMetadata
 from vllm.config import CacheConfig, LoRAConfig
@@ -55,7 +56,6 @@ from vllm.utils import is_hip
 
 from .interfaces import SupportsLoRA
 from .utils import PPMissingLayer, is_pp_missing_parameter, make_layers
-import vllm.envs as envs
 
 
 class LlamaMLP(nn.Module):
