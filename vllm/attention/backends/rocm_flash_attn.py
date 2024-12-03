566ebdbc9a25376fd611ec00b107e610425508f9
Hui Liu <96135754+hliuca@users.noreply.github.com>
2024-12-03 15:36:37 -0800

    use 0.0 instead of 0

--- vllm/attention/backends/rocm_flash_attn.py
+++ vllm/attention/backends/rocm_flash_attn.py
@@ -462,7 +462,7 @@ class ROCmFlashAttentionImpl(AttentionImpl):
 
         if logits_soft_cap is None:
             # In flash-attn, setting logits_soft_cap as 0 means no soft cap.
-            self.logits_soft_cap = 0
+            self.logits_soft_cap = 0.0
         else:
             self.logits_soft_cap = logits_soft_cap
 
