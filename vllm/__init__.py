cd49e47c75e48cce32376c6998eb9f3ce2bf66ce
tjtanaa <tunjian.tan@embeddedllm.com>
2025-01-20 15:01:49 +0000

    add ptpc fp8 implementation with torch._scaled_mm rowwise

--- vllm/__init__.py
+++ vllm/__init__.py
@@ -3,8 +3,8 @@
 from vllm.engine.arg_utils import AsyncEngineArgs, EngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.engine.llm_engine import LLMEngine
-from vllm.entrypoints.llm import LLM
 from vllm.entrypoints.fast_sync_llm import FastSyncLLM
+from vllm.entrypoints.llm import LLM
 from vllm.executor.ray_utils import initialize_ray_cluster
 from vllm.inputs import PromptType, TextPrompt, TokensPrompt
 from vllm.model_executor.models import ModelRegistry
